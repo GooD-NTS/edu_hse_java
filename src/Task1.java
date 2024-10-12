b8965a0abc7dcb60c1928a91efe662e8f338fc12
Yuriy Krivoruchko <ykrivoruchko@iikosoftware.com>
2024-10-12 09:48:03 +0300

    init + Task 1

---
+++ src/Task1.java
@@ -0,0 +1,25 @@
+public class Task1 {
+// 14. Написать программу, получающую на вход в качестве аргумента два параметра–числа x и y.
+// Если произведение этих чисел больше 100, то вычислить удвоенный куб первого числа
+// и второе число разделить на 2. Вывести результат на экран.
+    public static void main(String[] args) {
+        if (args.length == 2) {
+            var x = Integer.parseInt(args[0]);
+            var y = Integer.parseInt(args[1]);
+            System.out.println("x = " + x);
+            System.out.println("y = " + y);
+
+            if (x + y > 100) {
+                int xDC = x * x * x * x;
+                System.out.println("Удвоенный куб числа " + x + " = " + xDC);
+
+                var yDiv = (float)y / 2;
+                System.out.println("Число " + y + " зарделенное на 2 = " + yDiv);
+            } else {
+                System.out.println("Sum is less 100");
+            }
+        } else {
+            System.out.println("Bad args");
+        }
+    }
+}
